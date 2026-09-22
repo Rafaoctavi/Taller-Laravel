db00619471395c393abffb5f5427c99cb4700f0e
Rafaoctavi <roctavioj@gmail.com>
2026-09-22 14:46:05 -0400

    Primer commit: CRUD de tareas en laravel 12

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TareaController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Ruta resource: genera las 7 rutas CRUD automáticamente
+Route::resource('tareas', TareaController::class);
+
